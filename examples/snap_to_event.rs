593926a95880ba84e00d85f1951d836066b8ce5f
Belousovnm <belousov.nm@phystech.edu>
2024-08-27 01:46:45 +0300

    fix: results

--- examples/snap_to_event.rs
+++ examples/snap_to_event.rs
@@ -4,13 +4,13 @@ use orderbook::{
 };
 
 fn main() {
-    let ob_path = "/opt/Zenpy/jupyter/data/voskhod/RUST_OB/ob_SBER.2023-11-16.csv";
-    let orders_path = "/opt/Zenpy/jupyter/data/voskhod/RUST_OB/ob_SBER.2023-11-16.csv";
+    let ob_path = "/opt/Zenpy/jupyter/data/voskhod/RUST_OB/ob_ALRS.2024-01-29.csv";
+    let orders_path = "/opt/Zenpy/jupyter/data/voskhod/RUST_OB/orders_ALRS.2024-01-29.csv";
     let mut ob = OrderBook::new();
     let mut strat = Strategy::new(StrategyName::TestStrategy);
     let initial_balance = 0;
-    strat.buy_criterion = -0.0001;
-    strat.sell_criterion = 0.0001;
+    strat.buy_criterion = -0.0002;
+    strat.sell_criterion = 0.0002;
     strat.buy_position_limit = 100;
     strat.sell_position_limit = -100;
     strat.qty = 100;
