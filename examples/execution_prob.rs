2283fac140571eda8c5b2f9aa1f5d37635b2fb2b
Belousovnm <belousov.nm@phystech.edu>
2024-09-16 12:38:22 +0300

    clean test merge

--- examples/execution_prob.rs
+++ examples/execution_prob.rs
@@ -4,8 +4,8 @@ use orderbook::{
 };
 
 fn main() {
-    let ob_path = "/opt/Zenpy/jupyter/data/voskhod/RUST_OB/ob_ALRS.2024-01-29.csv";
-    let orders_path = "/opt/Zenpy/jupyter/data/voskhod/RUST_OB/orders_ALRS.2024-01-29.csv";
+    let ob_path = "data/ob.csv";
+    let orders_path = "data/orders.csv";
     let mut ob = OrderBook::default();
     let mut strat = FixPriceStrategy {
         buy_tick_criterion: Some(0),
